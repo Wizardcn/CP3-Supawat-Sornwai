e37801f2242cb15b24791998b5d461c703b541e6
supawat123789 <50136036+supawat123789@users.noreply.github.com>
2019-05-02 15:48:59 +0700

    added Exercise4

---
+++ Exercise4_Supawat_S.py
@@ -0,0 +1,9 @@
+foundationEn = 58
+generalBusiness = 69.5
+introToComSys = 75
+comProgramming = 95
+print('------------- Your Score -------------')
+print('Foundation English               :', foundationEn)
+print('General Business                 :', generalBusiness)
+print('Introduction to Computer Systems :', introToComSys)
+print('Computer Programming             :', comProgramming)
